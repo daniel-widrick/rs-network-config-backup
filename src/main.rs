31eee51de9d0cc8a642e0ff8c32db85f2bea5ab5
Daniel Widrick <daniel@widrick.net>
2023-03-22 21:16:25 -0400

    format fixes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+use ssh2::Session;
 use std::fs::File;
-use std::{fmt, io};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::path::Path;
-use chrono::{DateTime, Utc};
-use csv::StringRecord;
-use ssh2::Session;
-use serde::Deserialize;
+use std::{fmt, io};
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -20,14 +19,14 @@ struct HostRecord {
 #[derive(Debug)]
 enum BackupError {
     IOError(io::Error),
-    SSHError(ssh2::Error)
+    SSHError(ssh2::Error),
 }
 
 impl fmt::Display for BackupError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            BackupError::IOError(io_error) => write!(f,"{}",io_error),
-            BackupError::SSHError(ssh_error) => write!(f,"{}",ssh_error),
+            BackupError::IOError(io_error) => write!(f, "{}", io_error),
+            BackupError::SSHError(ssh_error) => write!(f, "{}", ssh_error),
         }
     }
 }
@@ -44,8 +43,6 @@ impl From<ssh2::Error> for BackupError {
 
 fn main() {
     println!("Hello, world!");
-
-
     //Load hosts from csv file
     let config_file_name = "hosts.csv";
     let config_file = File::open(config_file_name).unwrap();
@@ -53,18 +50,17 @@ fn main() {
         .has_headers(true)
         .from_reader(config_file);
 
-    let headers = config_reader.headers().unwrap();
     for result in config_reader.deserialize() {
-        let record : HostRecord = result.unwrap();
-        match BackupHost(&record) {
-            Ok(..) => println!("{} Backed up",record.name),
-            Err(error) => println!("Backup Failed: {} :: {}",record.name,error),
+        let record: HostRecord = result.unwrap();
+        match backup_host(&record) {
+            Ok(..) => println!("{} Backed up", record.name),
+            Err(error) => println!("Backup Failed: {} :: {}", record.name, error),
         };
     }
 }
 
-fn BackupHost(host_record : &HostRecord) -> Result<(), BackupError>{
-    let backup_file_name = MakeBackupFileName(&host_record.name);
+fn backup_host(host_record: &HostRecord) -> Result<(), BackupError> {
+    let backup_file_name = make_backup_file_name(&host_record.name);
 
     //Establish SSH Connection
     let tcp = TcpStream::connect(&host_record.address)?;
@@ -74,28 +70,31 @@ fn BackupHost(host_record : &HostRecord) -> Result<(), BackupError>{
     //Authenticate
     sess.userauth_password(&host_record.username, &host_record.password)?;
     //Run backup
-    let backup_cmd = format!("/system/backup/save name={} dont-encrypt=yes",backup_file_name);
+    let backup_cmd = format!(
+        "/system/backup/save name={} dont-encrypt=yes",
+        backup_file_name
+    );
     let mut channel = sess.channel_session()?;
     channel.exec(&backup_cmd)?;
 
-    return FetchBackup(&backup_file_name, &sess);
+    return fetch_backup(&backup_file_name, &sess);
 }
-fn MakeBackupFileName(hostname : &str) -> String {
-    let now:DateTime<Utc> = Utc::now();
+fn make_backup_file_name(hostname: &str) -> String {
+    let now: DateTime<Utc> = Utc::now();
     let date_stamp = format!("{}", now.format("%Y%m%d-%H%M"));
-    return format!("{}_{}.backup",hostname,date_stamp);
+    return format!("{}_{}.backup", hostname, date_stamp);
 }
-fn FetchBackup(filename : &str, sess : &Session) -> Result<(), BackupError>{
-    let (mut remote_file,stat) = sess.scp_recv(Path::new(filename))?;
-    let mut contents : Vec<u8> = Vec::new();
-    remote_file.read_to_end( (&mut contents))?;
+fn fetch_backup(filename: &str, sess: &Session) -> Result<(), BackupError> {
+    let (mut remote_file, _) = sess.scp_recv(Path::new(filename))?;
+    let mut contents: Vec<u8> = Vec::new();
+    remote_file.read_to_end(&mut contents)?;
     remote_file.send_eof()?;
     remote_file.wait_eof()?;
     remote_file.close()?;
     remote_file.wait_close()?;
 
     std::fs::create_dir_all("backups")?;
-    let file_path = format!("{}/{}","backups",filename);
+    let file_path = format!("{}/{}", "backups", filename);
     let mut file = File::create(file_path)?;
     file.write_all(contents.as_slice())?;
     return Ok(());
