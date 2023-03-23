0ef92f214abc2490883b3ec39473e0f661558d7b
Daniel Widrick <daniel@widrick.net>
2023-03-22 21:10:50 -0400

    refactor to load hosts from config

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,102 @@
 use std::fs::File;
+use std::{fmt, io};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::path::Path;
 use chrono::{DateTime, Utc};
+use csv::StringRecord;
 use ssh2::Session;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct HostRecord {
+    name: String,
+    address: String,
+    username: String,
+    password: String,
+}
+
+#[derive(Debug)]
+enum BackupError {
+    IOError(io::Error),
+    SSHError(ssh2::Error)
+}
+
+impl fmt::Display for BackupError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BackupError::IOError(io_error) => write!(f,"{}",io_error),
+            BackupError::SSHError(ssh_error) => write!(f,"{}",ssh_error),
+        }
+    }
+}
+impl From<io::Error> for BackupError {
+    fn from(error: io::Error) -> Self {
+        BackupError::IOError(error)
+    }
+}
+impl From<ssh2::Error> for BackupError {
+    fn from(error: ssh2::Error) -> Self {
+        BackupError::SSHError(error)
+    }
+}
 
 fn main() {
     println!("Hello, world!");
 
-    //Get date for filename
+
+    //Load hosts from csv file
+    let config_file_name = "hosts.csv";
+    let config_file = File::open(config_file_name).unwrap();
+    let mut config_reader = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(config_file);
+
+    let headers = config_reader.headers().unwrap();
+    for result in config_reader.deserialize() {
+        let record : HostRecord = result.unwrap();
+        match BackupHost(&record) {
+            Ok(..) => println!("{} Backed up",record.name),
+            Err(error) => println!("Backup Failed: {} :: {}",record.name,error),
+        };
+    }
+}
+
+fn BackupHost(host_record : &HostRecord) -> Result<(), BackupError>{
+    let backup_file_name = MakeBackupFileName(&host_record.name);
+
+    //Establish SSH Connection
+    let tcp = TcpStream::connect(&host_record.address)?;
+    let mut sess = Session::new()?;
+    sess.set_tcp_stream(tcp);
+    sess.handshake()?;
+    //Authenticate
+    sess.userauth_password(&host_record.username, &host_record.password)?;
+    //Run backup
+    let backup_cmd = format!("/system/backup/save name={} dont-encrypt=yes",backup_file_name);
+    let mut channel = sess.channel_session()?;
+    channel.exec(&backup_cmd)?;
+
+    return FetchBackup(&backup_file_name, &sess);
+}
+fn MakeBackupFileName(hostname : &str) -> String {
     let now:DateTime<Utc> = Utc::now();
     let date_stamp = format!("{}", now.format("%Y%m%d-%H%M"));
-    let mut password = File::open("password").unwrap();
-    let mut password_text : Vec<u8> = Vec::new();
-    password.read_to_end(&mut password_text).unwrap();
+    return format!("{}_{}.backup",hostname,date_stamp);
+}
+fn FetchBackup(filename : &str, sess : &Session) -> Result<(), BackupError>{
+    let (mut remote_file,stat) = sess.scp_recv(Path::new(filename))?;
+    let mut contents : Vec<u8> = Vec::new();
+    remote_file.read_to_end( (&mut contents))?;
+    remote_file.send_eof()?;
+    remote_file.wait_eof()?;
+    remote_file.close()?;
+    remote_file.wait_close()?;
 
-    let tcp = TcpStream::connect("192.168.67.1:22").unwrap();
-    let mut sess = Session::new().unwrap();
-    sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
-    sess.userauth_password("admin",std::str::from_utf8(&password_text).unwrap()).unwrap();
-
-    let mut channel = sess.channel_session().unwrap();
-    let filename = format!("102-seymour-rb4011-{}.backup",date_stamp);
-    let backup_cmd = format!("/system/backup/save name={} dont-encrypt=yes",filename);
-    channel.exec(&backup_cmd).unwrap();
-
-    let (mut remote_file,stat) = sess.scp_recv(Path::new(&filename)).unwrap();
-    println!("File Size: {} :: {}",filename,stat.size());
-    let mut contents = Vec::new();
-    remote_file.read_to_end(&mut contents).unwrap();
-    remote_file.send_eof();
-    remote_file.wait_eof();
-    remote_file.close();
-    remote_file.wait_close().unwrap();
-
-    let mut file = File::create(filename).unwrap();
-    file.write_all(contents.as_slice()).unwrap();
-
+    std::fs::create_dir_all("backups")?;
+    let file_path = format!("{}/{}","backups",filename);
+    let mut file = File::create(file_path)?;
+    file.write_all(contents.as_slice())?;
+    return Ok(());
 }
